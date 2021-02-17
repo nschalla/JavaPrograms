56df28847cdcc98def05379c6ad41e39d8f4f335
NarayanaSwamyChalla <nschalla6699@gmail.com>
2021-02-17 14:49:46 +0530

    some changes

--- Basic_programs/Hello.java
+++ Basic_programs/Hello.java
@@ -1,20 +1,12 @@
-class CountDigits
+//Program prints Hello in java
+class Hello
 {
-	int count =0;
-	public static int countDigitofNumber(int number)
-	{
-		while(number!=0)
-		{
-			number=number/10;
-			count++;
-		}
-		return count;
-	}
+	
 public static void main(String args[])
 {
- int n=520;
- System.out.println(countDigit.countDigitofNumber(n));
+ 
+ System.out.println("Hello");
 
 
 
-}
+}}
